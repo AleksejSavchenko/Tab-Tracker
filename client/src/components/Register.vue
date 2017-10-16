66f1bb97bc11ec1a7f8e2762df1a16db84b45d1b
Fenix <savchenko@evergreens.in.ua>
2017-10-16 21:04:04 +0300

    Commit first part. Client and server parts was maked. And also simple registration page.

---
+++ client/src/components/Register.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="">
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="Email" />
+    <br>
+    <input
+        type="password"
+        name="password"
+        v-model="password"
+        placeholder="Password" />
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log('Your data ', this.email + ' ' + this.password)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
